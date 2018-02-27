5a92bac911a2163fb69bf47d4e9e3011c0ff92fa
OmarHashim <36452748+OmarHashim@users.noreply.github.com>
2018-02-27 14:40:14 +0200

    Update 1599.cpp

--- Timus/1599.cpp
+++ Timus/1599.cpp
@@ -1,4 +1,4 @@
-//WA
+//TLE
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -20,6 +20,7 @@ typedef complex<long double> point;
 #define polar(r,t) ((r)*exp(point(0,(t))))
 #define angle(v) (atan2((v).Y,(v).X))
 #define length(v) ((long double)hypot((v).Y,(v).X))
+#define length2(v) (sqrt(v.X*v.X+v.Y*v.Y))
 #define lengthSqr(v) (dot(v,v))
 #define dot(a,b) ((conj(a)*(b)).real())
 #define cross(a,b) ((conj(a)*(b)).imag())
@@ -30,7 +31,7 @@ typedef complex<long double> point;
 #define same(a,b) (lengthSqr(vec(a,b))<EPS)
 #define mid(a,b) (((a)+(b))/point(2,0))
 #define perp(a) (point(-(a).Y,(a).X))
-const double PI=acos(-1.0);
+const long double PI=acos(-1.0);
 
 bool read(point & p){
 	double x,y;
@@ -40,21 +41,15 @@ bool read(point & p){
 	}
 	return 0;
 }
-bool eq(long double a,long double b){
-	return fabs(a-b)<EPS;
-}
-bool pointOnLine(const point& a, const point& b, const point& p) {
-	return fabs(cross(vec(a,b),vec(a,p))) < EPS;
-}
-bool pointOnSegment(point a,point b,point p){
-	return eq(length(vec(a,b)),length(vec(a,p))+length(vec(b,p)));
-}
-
 const int N=5010;
 int n,m;
 point arr[N];
 point p;
-
+inline long double fix(long double v){
+	if(v<-1)return -1;
+	if(v>1)return 1;
+	return v;
+}
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("i.txt","r",stdin);
@@ -64,25 +59,28 @@ int main(){
 	sc(m);
 	lop(i,m){
 		read(p);
-		double out=0;
+		long double out=0;
 		bool any=0;
 		lop(i,n){
 			point a=arr[i];
-			point b=arr[(i+1)%n];
-			if(pointOnSegment(a,b,p)){
-				any=1;
-				continue;
-			}
-			if(pointOnLine(a,b,p))continue;
-			point pa=normalize(vec(p,a));
-			point pb=normalize(vec(p,b));
-			double theta=acos(dot(pa,pb));
-			if(cross(pa,pb)>=0)out+=theta;
+			point b=(i==n-1?arr[0]:arr[i+1]);
+			point ab=vec(a,b);
+			point pa=vec(p,a);
+			point pb=vec(p,b);
+			long double lpa=length(pa);
+			long double lpb=length(pb);
+				if(fabs(length(ab)-lpa-lpb)<EPS){
+					any=1;
+					break;
+				}
+			pa=pa/lpa;
+			pb=pb/lpb;
+			point con=conj(pa)*(pb);
+			long double theta=acos(fix(con.X)); // dot product
+			if(con.Y>=0)out+=theta; // cross product
 			else out-=theta;
 		}
 		if(any)puts("EDGE");
-		else cout<<int(out/(2*PI))<<endl;
+		else printf("%d\n",int(out/(2*PI)));
 	}
-
-
 }
